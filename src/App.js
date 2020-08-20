cc249e9471d30f3d5c458d0c4d84bceb8e76b208
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-08-20 21:14:41 +0200

    add basic html structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>witam</p>
+    <main className='container'>
+      <header>
+        <h1>Lista zadań</h1>
       </header>
-    </div>
+
+      <section className='section'>
+        <header className='section__header'>
+          <h2 className='section__title'>Dodaj nowe zadanie</h2>
+        </header>
+        <div className='section__body'>
+          <form className='form'>
+            <input className='form__input' placeholder='co jest do zrobienia?' />
+            <button className='form__button'>Dodaj zadanie</button>
+          </form>
+        </div>
+      </section>
+      <section className='section'>
+        <header className='section__header'>
+          <h2 className='section__title'>Lista zadań</h2>
+          <div className='buttons'></div>
+        </header>
+        <div className='section__body'>
+          <ul className='tasks'></ul>
+        </div>
+      </section>
+    </main>
   );
 }
 
