914c67e4b10b21993ad874d2b0da4f64449bfd21
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-08-24 12:02:13 +0200

    add styles for Tasks component

--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Tasks.css';
 
 const Tasks = (props) => (
   <ul className='tasks'>
     {props.tasks.map(task => (
-      <li>
-        {task.content}
+      <li className={`tasks__item ${task.done && props.hideDoneTasks} ? ' tasks__item--hiden'`} key={task.id}>
+        <button className={'tasks__button tasks__button--toggleDone'}>{task.done ? 'ok' : ''}</button>
+        <span className={`tasks__content ${task.done ? 'tasks__content--done': ''}`}>{task.content}</span>
+        <button className={'tasks__button tasks__button--remove'}>kosz</button>
       </li>
     ))}
   </ul>
