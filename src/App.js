dc5da5cce6436b2f1f31b472b23a9a520a8edf0d
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-08-25 12:16:23 +0200

    add Section component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from './Form/Form.js';
 import Tasks from './Tasks/Tasks.js'
 import Buttons from './Buttons/Buttons.js'
+import Section from './Section/Section';
 
 const tasks = [
   { id: 1, content: 'kupić cytryny', done: true },
@@ -17,24 +18,15 @@ function App() {
       <header>
         <h1>Lista zadań</h1>
       </header>
-
-      <section className='section'>
-        <header className='section__header'>
-          <h2 className='section__title'>Dodaj nowe zadanie</h2>
-        </header>
-        <div className='section__body'>
-          <Form />
-        </div>
-      </section>
-      <section className='section'>
-        <header className='section__header'>
-          <h2 className='section__title'>Lista zadań</h2>
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}/>
-        </header>
-        <div className='section__body'>
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}/>
-        </div>
-      </section>
+    <Section 
+      title='Dodaj zadanie' 
+      body={<Form />}
+    />
+    <Section 
+      title='Lista zadań'
+      body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}/>}
+      extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}/>}
+    />
     </main>
   );
 }
