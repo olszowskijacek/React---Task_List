137d3c9b451be75f1848b032e6d541c77d928a08
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-08-28 09:30:01 +0200

    Updates

---
+++ precache-manifest.8c952192b262be6c5f95dcd019380fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c26f58aeda963e91ad82cf58ec30be",
+    "url": "/React---Task_List/index.html"
+  },
+  {
+    "revision": "b9ec67125b01f9153e62",
+    "url": "/React---Task_List/static/css/main.cc35449a.chunk.css"
+  },
+  {
+    "revision": "ccacd343875740c07808",
+    "url": "/React---Task_List/static/js/2.f634d82f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React---Task_List/static/js/2.f634d82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ec67125b01f9153e62",
+    "url": "/React---Task_List/static/js/main.4cf9b7bd.chunk.js"
+  },
+  {
+    "revision": "55eb09ef1982354c4572",
+    "url": "/React---Task_List/static/js/runtime-main.40bb2e6e.js"
+  }
+]);
